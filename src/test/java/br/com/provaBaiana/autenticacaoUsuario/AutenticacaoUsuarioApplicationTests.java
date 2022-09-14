6ccaf0db42ad53ee297bdda78a900bc15c306e00
Leonam Sousa <leonamgm18@gmail.com>
2022-09-14 09:43:17 -0300

    First Commit

---
+++ src/test/java/br/com/provaBaiana/autenticacaoUsuario/AutenticacaoUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.provaBaiana.autenticacaoUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacaoUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
